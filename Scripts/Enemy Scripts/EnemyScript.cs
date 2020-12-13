ae875af05039e8c5b825ca88b3188fe13bf16582
RITHEESH R <71439671+RITHEESH123@users.noreply.github.com>
2020-12-13 23:39:59 +0530

    Project Assets Repo
    In this I have attached a zip folder of my Assets which contains the complete assets of my project. Also I have attached the scripts separately for your quick reference on all my C# codes.

---
+++ Scripts/Enemy Scripts/EnemyScript.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float speed = 5f;
+    public float rotate_Speed = 50f;
+
+    public bool canShoot;
+    public bool canRotate;
+    private bool canMove = true;
+
+    public float bound_Y = -8f;
+
+    public Transform attack_Point;
+    public GameObject bullet_Prefab;
+
+    private Animator anim;
+    private AudioSource explosionSound;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        explosionSound = GetComponent<AudioSource>();
+        
+    }
+
+    void Start()
+    {
+        if (canRotate)
+        {
+            if (Random.Range(0, 2) > 0)
+            {
+                rotate_Speed = Random.Range(rotate_Speed, rotate_Speed + 20f);
+                rotate_Speed *= -1f;
+            }
+        }
+
+        if (canShoot)
+        {
+            Invoke("StartShooting", Random.Range(1f, 3f));
+        }
+    }
+
+    void Update()
+    {
+        Move();
+        RotateEnemy();
+    }
+
+    void Move()
+    {
+        if (canMove)
+        {
+            Vector3 temp = transform.position;
+            temp.y -= speed * Time.deltaTime;
+            transform.position = temp;
+
+            if (temp.y < bound_Y)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void RotateEnemy()
+    {
+        if (canRotate)
+        {
+            transform.Rotate(new Vector3(0f, 0f, rotate_Speed * Time.deltaTime), Space.World);
+        }
+    }
+
+    void StartShooting()
+    {
+        GameObject bullet = Instantiate(bullet_Prefab, attack_Point.position, Quaternion.Euler(0, 0, 90f));
+        bullet.GetComponent<BulletScript>().is_EnemyBullet = true;
+
+        if (canShoot)
+        {
+            Invoke("StartShooting", Random.Range(1f, 3f));
+        }
+    }
+
+    void TurnOffGameObject()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Bullet")
+        {
+            canMove = false;
+
+            if (canShoot)
+            {
+                canShoot = false;
+                CancelInvoke("StartShooting");
+            }
+
+            Invoke("TurnOffGameObject", 1f);
+
+            //play explosion sound
+            explosionSound.Play();
+
+            //play destroy animation
+            anim.Play("Destroy");
+        }
+    }
+
+}//@By RITHEESH
---
+++ Scripts/Enemy Scripts/EnemySpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float min_X = -8f, max_X = 8f;
+
+    public GameObject[] asteroid_Prefabs;
+    public GameObject enemyPrefab;
+
+    public float timer = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemies();
+    }
+
+    void SpawnEnemies()
+    {
+        float pos_X = Random.Range(min_X, max_X);
+        Vector3 temp = transform.position;
+        temp.x = pos_X;
+
+        if (Random.Range(0, 2) > 0)
+        {
+            Instantiate(asteroid_Prefabs[Random.Range(0, asteroid_Prefabs.Length)], temp, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(enemyPrefab, temp, Quaternion.Euler(0f, 0f, 180f));
+        }
+
+        Invoke("SpawnEnemies", timer);
+    }
+
+}//@BY RITHEESH
